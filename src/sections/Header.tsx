49791fa0a7ef88265a916d4ee5749f83de3a4738
muhammadatoqeer <muhammadanstoqeer@gmail.com>
2024-09-15 15:51:08 +0500

    setting up the project

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,3 +1,10 @@
 export const Header = () => {
-  return <div>Header Section</div>;
+  return <div>
+    <nav className="flex justify-center items-center relative top-3">
+      <a href="#" className="px-4  py-1.5 rounded-full text-white/70 text-sm font-semibold">Home</a>
+      <a href="#">Projects</a>
+      <a href="#">About</a>
+      <a href="#">Contact</a>
+    </nav>
+  </div>;
 };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -20,7 +20,10 @@ const config: Config = {
         md: "2rem",
       },
     },
-    extend: {},
+    extend: {
+      sans: 'var(--font-sans)',
+      serif: 'var(--font-serif)',
+    },
   },
   plugins: [],
 };
